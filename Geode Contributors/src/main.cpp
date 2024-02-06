f2bfe90e97ed8fb43ddc2497741c313ca419d0f1
BasilLmao <153382689+Uproxide@users.noreply.github.com>
2024-02-06 18:04:30 -0500

    Geode Contributors 1.1.0

--- Geode Contributors/src/main.cpp
+++ Geode Contributors/src/main.cpp
@@ -2,64 +2,90 @@
 #include <Geode/modify/ProfilePage.hpp>
 #include <Geode/binding/ProfilePage.hpp>
 #include <Geode/Bindings.hpp>
+#include <Geode/utils/web.hpp>
 #include <Geode/loader/Loader.hpp>
 #include <algorithm>
+#include <set>
 #include <cctype>
+#include <matjson.hpp>
 
 using namespace geode::prelude;
 
 // sorry for the mess, im not the c++ coder lmfao
 
-class buttonfunctionthing : public CCLayer {
-	public:
-	void onButtonClick(CCObject* sender) {
-		FLAlertLayer::create(
-			"About",
-			"This badge is given to <cp>Geode Contributors</c>. If you have contributed to geode in anyway (Bindings, Main Geode Loader/Mod, Etc...), You will obtain this badge. (Don't have the Badge? Read the about.md!)",
-			"OK"
-		)->show();
-	}
-};
+std::set<int> geodeContributors;
+bool gotTheShit = false;
 
 class $modify(ProfilePage) {
-	bool m_hasBeenOpened = false;
+public:
+    bool loadedOnce = false;
 
-	bool init(int p0, bool p1) {
-		if (!ProfilePage::init(p0, p1)) return false;
+    bool init(int p0, bool p1) {
+        if (!ProfilePage::init(p0, p1)) return false;
 
-		std::set<int> geodeContributors = {25397826, 5568872, 3516725, 11535118, 761691, 104257, 807756, 16778880};
-																									
-
-		m_mainLayer->removeChildByID("my-stuff-hint");
-		auto mainMenu = static_cast<cocos2d::CCLayer*>(this->getChildByIDRecursive("main-menu"));
-		auto spr = CCSprite::create("geode.png"_spr);
-		auto btn = CCMenuItemSpriteExtra::create(
-       		spr, this, menu_selector(buttonfunctionthing::onButtonClick)
-    	);
-		auto contentSize = mainMenu->getContentSize();
-
-
-		btn->setPosition(375.500, -11);
-		btn->setZOrder(26);
-		btn->setID("geode-badge");
-		mainMenu->addChild(btn);
-		btn->setVisible(false);
-
-		std::string username = m_usernameLabel->getString();
-
-		if (geodeContributors.count(p0) > 0) 
+		if (!gotTheShit)
 		{
-			btn->setVisible(true);
-        }
-		else
-		{
-			// how many times do we have to tell you this old man
+			auto res = web::fetch("https://raw.githubusercontent.com/Uproxide/GeodeMods/main/other/contributors.txt");
+			if (!res) {
+				log::info("Failed to fetch Contributors! Your internet is probably hot ass rn :ned:");
+			}
+			else {
+				auto data = res.value();
+			
+				std::istringstream iss(data);
+				int tempid;
+
+				while (iss >> tempid) {
+					geodeContributors.insert(tempid);
+				}
+				gotTheShit = true;
+			}
 		}
 
-		auto posX = m_usernameLabel->getPositionX();
-		auto posY = m_usernameLabel->getPositionY();
+        
+															
 
+		for (const auto& contributor : geodeContributors) {
+			if (contributor == p0) {
+				CCNode* first_letter = reinterpret_cast<CCNode*>(m_usernameLabel->getChildren()->objectAtIndex(0));
+				auto spr = CCSprite::create("geode.png"_spr);
+				auto layerIWant = static_cast<cocos2d::CCLayer*>(this->getChildren()->objectAtIndex(0));
+				spr->setScale(1.1f); // much better
+				spr->setPosition(first_letter->convertToWorldSpace(getPosition()));
+				spr->setPosition({spr->getPositionX() - 15.f, spr->getPositionY() + 10.f});
+				spr->setID("geode-badge");
+				layerIWant->addChild(spr);
 
+				auto ytbadge = static_cast<cocos2d::CCSprite*>(this->getChildren()->objectAtIndex(1));
+
+				if (Loader::get()->isModLoaded("xanii.youtubers") && ytbadge) {
+					ytbadge->setScale(0.4f);
+					ytbadge->setPosition(ccp(220.4, 274.6));
+					ytbadge->setZOrder(1);
+				}
+				else
+				{
+					// no error handling? this works too so erm
+				}
+			}
+		}
 		return true;
+	}
+
+	void setupPageInfo(gd::string name, char const* chars) {
+		ProfilePage::setupPageInfo(name,chars);
+
+		auto ytbadge = static_cast<cocos2d::CCSprite*>(this->getChildren()->objectAtIndex(1));
+		auto geodebadge = static_cast<cocos2d::CCSprite*>(this->getChildByID("geode-badge"));
+		
+
+		if (Loader::get()->isModLoaded("xanii.youtubers") && ytbadge && geodebadge) {
+			ytbadge->setScale(0.4f);
+			ytbadge->setPosition(ccp(220.4, 274.6));
+		}
+		else
+		{
+			// no error handling?
+		}
 	}
 };
